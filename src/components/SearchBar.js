7ffab7706d2f9a10fa35b409c61bbcc98e9018ae
M17ggie <102402171+M17ggie@users.noreply.github.com>
2022-06-08 23:07:34 +0530

    Delete SearchBar.js

--- src/components/SearchBar.js
+++
@@ -1,9 +0,0 @@
-import styles from './SearchBar.module.css'
-
-const SearchBar=()=>{
-    return <div className={styles.search}>
-        <input type='text' placeholder='Search for groceries and more...' />
-    </div>
-}
-
-export default SearchBar
