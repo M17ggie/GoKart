b726cec801f1f0858088674463a89af952c42d8d
Raj Mhatre <rmhatre799@gmail.com>
2022-07-16 22:20:32 +0530

    removed routes from Header.js

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -2,8 +2,6 @@ import { Button } from "react-bootstrap";
 import styles from './Header.module.css'
 import SearchBar from "./SearchBar";
 import { ReactComponent as CartIcon } from '../assets/cart.svg';
-import { Route } from "react-router-dom";
-
 
 const Header = (props) => {
 
@@ -14,19 +12,14 @@ const Header = (props) => {
         <SearchBar />
 
         <div>
-            <Route path='login'>
-                <Button onClick={props.onShowLogInModal} className={`btn-dark mx-2 ${styles['btn-log-in']}`}>Log-In</Button>
-            </Route>
-            <Route path='signup'>
-                <Button onClick={props.onShowSignUpModal} className={`btn-dark mx-2 ${styles['btn-sign-up']}`}>SignUp</Button>
-            </Route>
-            <Route path='cart'>
-                <CartIcon className={styles['cart-icon']} />
-            </Route>
+            {/* login button  */}
+            <Button onClick={props.onShowLogInModal} className={`btn-dark mx-2 ${styles['btn-log-in']}`}>Log-In</Button>
+            {/* signup button  */}
+            <Button onClick={props.onShowSignUpModal} className={`btn-dark mx-2 ${styles['btn-sign-up']}`}>SignUp</Button>
+            {/* cart button  */}
+            <CartIcon className={styles['cart-icon']} />
         </div>
-
     </div>
-
 };
 
 export default Header;
